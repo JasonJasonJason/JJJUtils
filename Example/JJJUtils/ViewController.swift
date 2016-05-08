046d190bb773a9af338126f0b7b1f3e7051672c3
Jason Harris <jasonharris1991@gmail.com>
2016-05-08 10:27:16 -0500

    Add 'getBlankAlertController' method

--- Example/JJJUtils/ViewController.swift
+++ Example/JJJUtils/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import JJJUtils
 
 class ViewController: UIViewController {
 
---
+++ JJJUtils/Classes/JJJUtils.swift
@@ -0,0 +1,28 @@
+//
+//  JJJUtils.swift
+//  Pods
+//
+//  Copyright © 2016 Jason Harris. All rights reserved.
+//
+
+
+import Foundation
+
+public class JJJUtils {
+    
+    /// Create information-only UIAlertController with 'OK' button and no delegate set
+    /// Example usage:
+    /// ```
+    /// let alertControler = JJJUtils.getBlankAlertController("title", message: "message")
+    /// self.presentViewController(alertControler, animated: true, completion: nil)
+    /// ```
+    public static func getBlankAlertController(title:String, message:String) -> UIAlertController {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        let button = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alert.addAction(button)
+        return alert
+        
+    }
+    
+}
