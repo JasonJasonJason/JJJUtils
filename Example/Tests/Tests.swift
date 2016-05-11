788c44b5cc18071cae5ffa2227cf0a8356397fb4
Jason Harris <jasonharris1991@gmail.com>
2016-05-11 10:45:29 -0500

    Add tests for 'randomSort' and 'loadImageForTest'

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -6,24 +6,31 @@ class Tests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testLoadImageForTest() {
+        
+        let llama = JJJUtils.loadImageForTest(self.dynamicType, imageName: "llama.jpg")
+        XCTAssertNotNil(llama, "Expected to see a glorious llama :-(")
+        
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
+    func testRandomSort() {
+        
+        let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        
+        for _ in 0 ..< 10 {
+            
+            let shuffledNums = JJJUtils.randomSort(nums)
+            
+            if shuffledNums != nums {
+                return
+            }
+            
         }
+        
+        XCTFail("nums should have been randomly sorted at least once, statistically speaking")
+        
     }
     
 }
